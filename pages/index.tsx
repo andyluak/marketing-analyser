fb8d6bfa78ad469bc593a79813880dd85b5d5107
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2023-12-30 12:50:53 +0200

    Add getServerSideProps for authentication check

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import { GetServerSideProps } from "next"
+import { buildClerkProps, getAuth } from "@clerk/nextjs/server"
+
 export default function Home() {
   return <section>Start Building</section>
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { userId } = getAuth(ctx.req)
+
+  if (!userId) {
+    return { redirect: { destination: "/sign-in", permanent: false } }
+  }
+
+  return { props: { ...buildClerkProps(ctx.req) } }
 }
